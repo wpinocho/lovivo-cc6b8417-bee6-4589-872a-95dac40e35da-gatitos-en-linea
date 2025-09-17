28cc195c37f8efe42c1f41b93a42a3d39ce4dcef
wpinocho <julian.ruiz.loza@gmail.com>
2025-09-17 17:42:22 -0600

    Initial commit

---
+++ src/lib/config.ts
@@ -0,0 +1,3 @@
+export const STORE_ID = 'ba5e7b47-7bdb-458c-95af-dc9dd758391a'
+export const STRIPE_PUBLISHABLE_KEY = 'pk_live_51Rk9SNP66p9BDoW4lgxDnuqvNZYEaL400FCX0GStvuaslkEMBZNRqln5M5bDcBSBmFmR7hlY0pDcA4u4VIi8CRIg00KkqRzlVs'
+export const LOVIVO_STRIPE_ACCOUNT_ID = 'acct_1Rk9SNP66p9BDoW4'
